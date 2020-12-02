893af1db6e6b76e96d1293d469fa1360cedd01c1
Kamil Demirtas <kamil@tolkido.com>
2020-12-02 13:57:32 +0300

    - CollectionView data fetch.

--- news_projectAlamofire/ApiManager.swift
+++ news_projectAlamofire/ApiManager.swift
@@ -7,28 +7,58 @@
 
 import Foundation
 import Alamofire
+import AlamofireObjectMapper
 
 class ApiManager  {
     
-    
     static let shared = ApiManager()
     
-    
-   private init() {
+    private init() {
         
     }
-    func fetchJsonFromApi() {
-        AF.request("http://newsapi.org/v2/top-headlines?"+"country=us&"+"apiKey=5369afde0eed4fba9cf5d96f50d35d29").responseString{
-            response in
-            switch(response.result){
-            case .success(let responseString):
-                print(responseString)
-                
-                let newsResponse = ApiResponse(JSONString:"\(responseString)" )
-                
-            case .failure(let error):
-                print(error)
+    
+//    func fetchJsonFromApi() {
+//        AF.request("http://newsapi.org/v2/top-headlines?"+"country=us&"+"apiKey=5369afde0eed4fba9cf5d96f50d35d29").responseString{
+//            response in
+//            switch(response.result){
+//            case .success(let responseString):
+//                print(responseString)
+//
+//                let newsResponse = ApiResponse(JSONString:"\(responseString)" )
+//
+//            case .failure(let error):
+//                print(error)
+//            }
+//        }
+//    }
+//
+    
+    
+    func newsRequest(completionHandler: @escaping (Int, Error?,[News]) -> ()) {
+        
+        AF.request("http://newsapi.org/v2/top-headlines?"+"country=us&"+"apiKey=5369afde0eed4fba9cf5d96f50d35d29",
+            method: .get,
+            encoding: JSONEncoding.default
+        ).validate().responseObject { (response: AFDataResponse<ApiResponse>) in
+            
+            if let status = response.response?.statusCode {
+                switch(status){
+                case 200:
+                    do {
+                        let result = try response.result.get()
+                        completionHandler(1, nil,result.articles!)
+                        print("Success")
+                    }
+                    catch {
+                        fatalError("unable to convert data to JSON")
+                    }
+                default:
+                  
+                    print("error with response status: \(status)")
+                }
             }
+            
+            
         }
     }
 }
--- news_projectAlamofire/ViewController.swift
+++ news_projectAlamofire/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 import Alamofire
 
 class ViewController: UIViewController {
+    
     var dataSourceArray = [News]()
     
     @IBOutlet weak var helloLbl: UILabel!
@@ -18,46 +19,49 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var searchBar: UISearchBar!
     
+//    newsCollectionView
     @IBOutlet weak var Collection: UICollectionView!
     
-   
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-      ApiManager.shared.fetchJsonFromApi()
-        makeRounded()
      
+        ApiManager.shared.newsRequest { (int, err, news) in
+            if err == nil {
+                self.dataSourceArray = news
+                self.Collection.reloadData()
+            }else{
+                print(err?.localizedDescription)
+            }
+          
         }
+        makeRounded()
+        
+    }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(true, animated: animated)
     }
-
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.setNavigationBarHidden(false, animated: animated)
     }
     
     
-    
-    
-    
-    
+    func makeRounded()  {
+        profileImage.layer.borderWidth = 1
+        profileImage.layer.masksToBounds = false
+        profileImage.layer.borderColor = UIColor.gray.cgColor
+        profileImage.layer.cornerRadius = profileImage.frame.height/2
+        profileImage.clipsToBounds = true
     }
+    
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+// MARK: CollectionView Ext.
 @available(iOS 12.0, *)
 extension ViewController : UICollectionViewDelegate , UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -67,7 +71,7 @@ extension ViewController : UICollectionViewDelegate , UICollectionViewDataSource
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCollectionViewCell
         cell.newsTitle.text = dataSourceArray[indexPath.row].title!
-        let url = URL(string: dataSourceArray[indexPath.row].urlToImage!)
+//        let url = URL(string: dataSourceArray[indexPath.row].urlToImage!)
         
         return cell
     }
@@ -77,17 +81,8 @@ extension ViewController : UICollectionViewDelegate , UICollectionViewDataSource
         
     }
     
-    
-    
-    func makeRounded()  {
-        profileImage.layer.borderWidth = 1
-           profileImage.layer.masksToBounds = false
-           profileImage.layer.borderColor = UIColor.gray.cgColor
-           profileImage.layer.cornerRadius = profileImage.frame.height/2
-           profileImage.clipsToBounds = true
-    }
-    }
-    
-    
+}
+
+
 
 
